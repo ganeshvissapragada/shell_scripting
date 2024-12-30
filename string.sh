ef8ab15346340886f68863e071f7e68f44590250
GaneshV <vissapragadaganesh1@gmail.com>
2024-12-30 08:27:52 +0530

    adding operation and conditional stmts

---
+++ string.sh
@@ -0,0 +1,15 @@
+myVar="Hey boy, How are you?"
+
+myVarLength=${#myVar}
+echo "Length of myVar is $myVarLength"
+
+
+echo "Upper case is -- ${myVar^^}"
+echo "Lower case is --  ${myVar,,}"
+
+#To replace a string
+newVar=${myVar/boy/Ganesh}
+echo "New Var is --$newVar"
+
+#To slice a string
+echo "After slice ${myVar:4:5}"
